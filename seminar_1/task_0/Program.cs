a02eeb434ae778b969adf242ee0f959ac66edc7b
Fedor <fdr@mail.ru>
2022-11-23 17:41:38 +0300

    add suare task_0

---
+++ seminar_1/task_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на входе принимает число а выдает его квадрат.
+// пример
+// 4 -> 16
+// -3-> 9
+// -7-> 49
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
