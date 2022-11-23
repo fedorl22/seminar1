e53a8c0c42906c96d7d9c060e4c6a132caa5fcdb
Fedor <fdr@mail.ru>
2022-11-23 18:01:35 +0300

    add test square test_1

---
+++ seminar_1/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//На входе принимает 2 числа и проверяет является ли перво число квадратом второго.
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b )
+{
+    Console.Write("YES!");
+}
+else
+{
+    Console.Write("NO!");
+}
