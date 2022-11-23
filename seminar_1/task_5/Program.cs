e5785520da615ae79147ba5e3e2c8b4a42f27c31
Fedor <fdr@mail.ru>
2022-11-23 18:27:29 +0300

    add task5

---
+++ seminar_1/task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число N, а на выходе -N до N
+
+
+
+Console.WriteLine("Введите число N: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -count; i < count+1; i++)
+{
+      Console.WriteLine(i);
+}
+
